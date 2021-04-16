1db94715233c2120bfe5f94ffc9516d8ef1be200
森田健太 <k_morita@arsaga.jp>
2021-04-16 11:45:59 +0900

    リアクティブデータ(反応するデータ)

--- js/index.js
+++ js/index.js
@@ -1,41 +1,19 @@
 let app = new Vue({
     el: '#app',
     data: {
-        message: 'message'
-    },
-    methods: {
-        onclick: function(){
-            this.message = 'メッセージ';
-        }
-    },
-    
-    beforeCreate() {
-        console.log('beforeCrearte...');
+        current: new Date()
     },
     created() {
-        console.log('created...');
+        // thisをthatに退避させているのは、setIntervalメソッドの配下では、
+        // thisは、Vueインスタンスではなく、グローバルオブジェクト(Window)に変化してしまうため。
+        let that = this;
+        this.timer = setInterval(function() {
+            that.current = new Date();
+        }, 1000);
     },
-    beforeMount() {
-        console.log('beforeMount...');
-    },
-    mounted() {
-        console.log('mounted...');
-    },
-    beforeUpdate() {
-        console.log('beforeUpdate...');
-    },
-    updated() {
-        console.log('updated...');
-    },
-    beforeDestroy() {
-        console.log('beforeDestroy...');
-    },
-    destroyed() {
-        console.log('destroyed...');
-    },
-});
 
-// 3000ミリ秒のあとに破棄
-setTimeout(function() {
-   app.$destroy(); 
-}, 3000);
+    beforeDestroy() {
+        clearInterval(this.timer);
+    },
+    
+});
