4bb217479cd97255d1827acca94b1f0fb2b49e46
森田健太 <k_morita@arsaga.jp>
2021-04-16 12:02:10 +0900

    リアクティブシステムの制約

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 let app = new Vue({
     el: '#app',
     data: {
-        current: new Date()
+        author: {
+            name: '山田'
+        }
     },
     created() {
         // thisをthatに退避させているのは、setIntervalメソッドの配下では、
         // thisは、Vueインスタンスではなく、グローバルオブジェクト(Window)に変化してしまうため。
         let that = this;
-        this.timer = setInterval(function() {
-            that.current = new Date();
+        this.timer = setTimeout(function() {
+            // 初期に設定されているプロパティの変更がないと、新しいプロパティの変更が検知されない。
+            // that.author.name = 'Y.YAMADA';
+            // that.author.company = 'WINGSプロジェクト';
+
+            // 以下の記述で新しいプロパティをVueに設定できる。ただし、後から追加できるのは入れ子となったオブジェクトの配下だけ。
+            // dataオブジェクト直下にはこのメソッドでプロパティを追加できない。
+            // プロパティを削除する場合は、removeメソッドを利用する。
+            that.$set(that.author, 'company', 'WINGSプロジェクト');
         }, 1000);
     },
 
