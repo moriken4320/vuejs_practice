bc9ce182990f3ec7a4b78eab68d72bb2e5788871
森田健太 <k_morita@arsaga.jp>
2021-04-16 15:15:52 +0900

    ウォッチャーについて

--- js/index.js
+++ js/index.js
@@ -1,30 +1,36 @@
 let app = new Vue({
-    el: '#app',
-    data: {
-        author: {
-            name: '山田'
-        }
-    },
-    mounted() {
-        let that = this;
-        this.$set(this.author, 'company', 'WINGSプロジェクト');
+  el: "#app",
+  data: {
+    name: "",
+    upperName: "",
+  },
 
-        // falseになる
-        console.log(that.$el.textContent.includes(that.author.company));
+  //   遅延処理用のdelayFuncメソッドを準備
+  created() {
+    this.delayFunc = _.debounce(this.getUpper, 2000);
+  },
 
-        // ビューへの反映を待つメソッド
-        this.$nextTick().then(function () {
-            // trueになる
-            console.log(that.$el.textContent.includes(that.author.company));
-        })
-        
-        Vue.nextTick(()=>{
-            console.log(that.$el.textContent.includes(that.author.company));
-        })
+  //   nameプロパティが変化した時にdelayFuncメソッドを呼び出し
+  watch: {
+    name: function (newValue, oldValue) {
+      this.delayFunc();
     },
 
-    beforeDestroy() {
-        clearInterval(this.timer);
+    // 入れ子プロパティの場合(クォートでくくる必要がある)
+    // 'author.name': function(newValue, oldValue){}
+  },
+
+  //   nameの値を大文字に変換したものをupperNameプロパティに設定
+  methods: {
+    getUpper: function () {
+      this.upperName = this.name.toUpperCase();
     },
-    
+  },
+
+  //   watchを算出プロパティに置き換えると賊座に変換される。
+  //   computed: {
+  //       upperName: function(){
+  //           return this.name.toUpperCase();
+  //       }
+  //   }
 });
