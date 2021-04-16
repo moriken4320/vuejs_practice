c470b7dcbc659ec56794600b35c0840066fbb77c
森田健太 <k_morita@arsaga.jp>
2021-04-16 12:28:31 +0900

    ビューの非同期更新を理解する

--- js/index.js
+++ js/index.js
@@ -5,20 +5,22 @@ let app = new Vue({
             name: '山田'
         }
     },
-    created() {
-        // thisをthatに退避させているのは、setIntervalメソッドの配下では、
-        // thisは、Vueインスタンスではなく、グローバルオブジェクト(Window)に変化してしまうため。
+    mounted() {
         let that = this;
-        this.timer = setTimeout(function() {
-            // 初期に設定されているプロパティの変更がないと、新しいプロパティの変更が検知されない。
-            // that.author.name = 'Y.YAMADA';
-            // that.author.company = 'WINGSプロジェクト';
+        this.$set(this.author, 'company', 'WINGSプロジェクト');
 
-            // 以下の記述で新しいプロパティをVueに設定できる。ただし、後から追加できるのは入れ子となったオブジェクトの配下だけ。
-            // dataオブジェクト直下にはこのメソッドでプロパティを追加できない。
-            // プロパティを削除する場合は、removeメソッドを利用する。
-            that.$set(that.author, 'company', 'WINGSプロジェクト');
-        }, 1000);
+        // falseになる
+        console.log(that.$el.textContent.includes(that.author.company));
+
+        // ビューへの反映を待つメソッド
+        this.$nextTick().then(function () {
+            // trueになる
+            console.log(that.$el.textContent.includes(that.author.company));
+        })
+        
+        Vue.nextTick(()=>{
+            console.log(that.$el.textContent.includes(that.author.company));
+        })
     },
 
     beforeDestroy() {
