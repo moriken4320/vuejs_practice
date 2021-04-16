63b425245111491a0f047cb2a879c407fae6744a
森田健太 <k_morita@arsaga.jp>
2021-04-16 15:50:05 +0900

    イベントハンドラー系ディレクティブ、イベントオブジェクト

--- js/index.js
+++ js/index.js
@@ -1,36 +1,13 @@
 let app = new Vue({
   el: "#app",
   data: {
-    name: "",
-    upperName: "",
+    message: "",
   },
 
-  //   遅延処理用のdelayFuncメソッドを準備
-  created() {
-    this.delayFunc = _.debounce(this.getUpper, 2000);
-  },
-
-  //   nameプロパティが変化した時にdelayFuncメソッドを呼び出し
-  watch: {
-    name: function (newValue, oldValue) {
-      this.delayFunc();
-    },
-
-    // 入れ子プロパティの場合(クォートでくくる必要がある)
-    // 'author.name': function(newValue, oldValue){}
-  },
-
-  //   nameの値を大文字に変換したものをupperNameプロパティに設定
   methods: {
-    getUpper: function () {
-      this.upperName = this.name.toUpperCase();
-    },
+    onclick: function(e, message){
+        this.message = message;
+        console.log(e.target);
+    }
   },
-
-  //   watchを算出プロパティに置き換えると賊座に変換される。
-  //   computed: {
-  //       upperName: function(){
-  //           return this.name.toUpperCase();
-  //       }
-  //   }
 });
