0857d5a50fd1d28b2fd9a4371e79921f39ef72bf
森田健太 <k_morita@arsaga.jp>
2021-04-16 10:28:59 +0900

    算出プロパティ

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@ let app = new Vue({
     data: {
         message: 'みなさん、こんにちは！',
         url: 'https://google.com',
-        flag: true
+        flag: true,
+        email: 'Y-Suzuki@example.com'
+    },
+    // 演算した結果を取得する算出プロパティ
+    computed: {
+        localEmail: function() {
+            return this.email.split('@')[0].toLowerCase();
+        }
     }
 });
