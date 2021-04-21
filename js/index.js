731c5d4e36bdbed8065d05bc9efad668c898e6a9
森田健太 <k_morita@arsaga.jp>
2021-04-21 17:46:28 +0900

    変更メソッド：配列の変更を反映する

--- js/index.js
+++ js/index.js
@@ -1,22 +1,12 @@
 let app = new Vue({
   el: "#app",
   data: {
-    songs: [
-        {
-            title: '赤とんぼ',
-            lyrics: '夕焼け小焼けの赤とんぼ。。。',
-            composer: '山田工作'
-        },
-        {
-            title: '青とんぼ',
-            lyrics: '夕焼け小焼けの青とんぼ。。。',
-            composer: '山田工作'
-        },
-        {
-            title: '黄とんぼ',
-            lyrics: '夕焼け小焼けの黄とんぼ。。。',
-            composer: '山田工作'
-        },
-    ]
+    list: ['赤パジャマ', '青パジャマ', '黄パジャマ']
+  },
+  methods: {
+      onclick: function(){
+        //   this.$set(this.list, 1, '茶パジャマ');
+          this.list.splice(1, 1, '茶パジャマ');
+      }
   },
 });
