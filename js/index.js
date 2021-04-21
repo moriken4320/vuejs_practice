b279f639177d463ac9257105c99d9fb87471027e
森田健太 <k_morita@arsaga.jp>
2021-04-21 11:47:44 +0900

    ファイル入力ボックス

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
 let app = new Vue({
   el: "#app",
   data: {
-    myName: "匿名",
-    pet: "いぬ",
-    agree: true,
-    os: ["Windows"],
-    oss: [],
-    unit: {},
+    message: ''
   },
   methods: {
     onchange: function(){
-          console.log(this.unit.name + '：' + this.unit.size);
+          // アップロードファイルを準備
+          let fl = this.$refs.upfile.files[0];
+          let data = new FormData();
+          data.append('upfile', fl, fl.name);
+          // サーバーにデータを送信
+          fetch('upload.php', {
+              method: 'POST',
+              body: data,
+          })
+          // 成功時には結果を表示
+          .then(function(response){
+              return response.text();
+          })
+          .then(function(text){
+              this.message = text;
+          })
+          // 失敗時にはエラーメッセージをダイアログ表示
+          .catch(function(error){
+              window.alert('Error:' + error.message);
+          })
         }
     }
 });
