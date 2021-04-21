07b3639dec83cdbc60f36131fdf79cf671c568fd
森田健太 <k_morita@arsaga.jp>
2021-04-21 11:54:48 +0900

    .number修飾子：入力値を数値としてバインドする

--- js/index.js
+++ js/index.js
@@ -1,30 +1,12 @@
 let app = new Vue({
   el: "#app",
   data: {
-    message: ''
+    temperature: 0,
   },
   methods: {
-    onchange: function(){
-          // アップロードファイルを準備
-          let fl = this.$refs.upfile.files[0];
-          let data = new FormData();
-          data.append('upfile', fl, fl.name);
-          // サーバーにデータを送信
-          fetch('upload.php', {
-              method: 'POST',
-              body: data,
-          })
-          // 成功時には結果を表示
-          .then(function(response){
-              return response.text();
-          })
-          .then(function(text){
-              this.message = text;
-          })
-          // 失敗時にはエラーメッセージをダイアログ表示
-          .catch(function(error){
-              window.alert('Error:' + error.message);
-          })
-        }
-    }
+    // 入力値を小数点以下１位に丸め、ログ出力
+    onchange: function () {
+        console.log(this.temperature.toFixed(1));
+    },
+  },
 });
