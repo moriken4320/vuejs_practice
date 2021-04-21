baff559ad02c464d58f1105870dd3e013b9dc70c
森田健太 <k_morita@arsaga.jp>
2021-04-21 17:09:31 +0900

    v-for：配列やオブジェクトを繰り返し処理する

--- js/index.js
+++ js/index.js
@@ -1,9 +1,34 @@
 let app = new Vue({
   el: "#app",
   data: {
-    show: true,
-    holiday: "",
-    pay: 'credit'
+    books: [
+      {
+        isbn: "978-4-7981-5757-1",
+        title: "JavaScript逆引きレシピ",
+        price: 2000,
+      },
+      {
+        isbn: "978-4-7981-5757-2",
+        title: "Ruby逆引きレシピ",
+        price: 1000,
+      },
+      {
+        isbn: "978-4-7981-5757-3",
+        title: "PHP逆引きレシピ",
+        price: 4000,
+      },
+    ],
+    book: {
+      isbn: "978-4-7981-5757-1",
+      title: "JavaScript逆引きレシピ",
+      price: 2000,
+    },
   },
-  methods: {},
+  computed: {
+      expensiveBooks: function(){
+          return this.books.filter(function(b){
+              return b.price >= 1500;
+          })
+      }
+  },
 });
