48b8f8f0a3aead818c01017d9d53f150689770bc
森田健太 <k_morita@arsaga.jp>
2021-04-21 18:41:39 +0900

    javascript式から属性値を決定する

--- js/index.js
+++ js/index.js
@@ -1,10 +1,7 @@
 let app = new Vue({
   el: "#app",
   data: {
-    attrs: {
-        size: 20,
-        maxlength: 14,
-        required: true //値を持たない属性を設定する場合、trueとしておく(falseの場合は属性が付与されない)
-    }
+    attr: 'width',
+    size: 100,
   },
 });
