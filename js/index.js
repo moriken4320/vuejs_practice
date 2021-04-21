ad3d549abb0844c404e25d11d2e87eefd0bb6bbd
森田健太 <k_morita@arsaga.jp>
2021-04-21 15:57:37 +0900

    .trim修飾子：入力値の前後の空白を除去する

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 let app = new Vue({
   el: "#app",
   data: {
-    temperature: 0,
+    memo: '',
   },
   methods: {
     // 入力値を小数点以下１位に丸め、ログ出力
     onchange: function () {
-        console.log(this.temperature.toFixed(1));
+        console.log('入力値は「' + this.memo + '」です。');
     },
   },
 });
