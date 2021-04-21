c4f9996f3976550f48e4bcbd542ce7283e897205
森田健太 <k_morita@arsaga.jp>
2021-04-21 17:57:56 +0900

    配列要素の追加/削除を効率的に行う

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 let app = new Vue({
   el: "#app",
   data: {
-    list: ['赤パジャマ', '青パジャマ', '黄パジャマ']
+    list: ["赤パジャマ", "青パジャマ", "黄パジャマ"],
   },
   methods: {
-      onclick: function(){
-        //   this.$set(this.list, 1, '茶パジャマ');
-          this.list.splice(1, 1, '茶パジャマ');
-      }
+    onclick: function () {
+      // 配列の先頭要素を削除
+      this.list.shift();
+    },
   },
 });
