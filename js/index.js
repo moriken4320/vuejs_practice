00a4a615fd4f0e2c45e0ad101613c22bde9a1323
森田健太 <k_morita@arsaga.jp>
2021-04-21 16:01:00 +0900

    .lazy修飾子：バインドのタイミングを遅延させる

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
 let app = new Vue({
   el: "#app",
   data: {
-    memo: '',
+    myName: '匿名',
   },
   methods: {
-    // 入力値を小数点以下１位に丸め、ログ出力
-    onchange: function () {
-        console.log('入力値は「' + this.memo + '」です。');
-    },
+    
   },
 });
